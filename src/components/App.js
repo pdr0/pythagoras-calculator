7e902e3d4e6ac7857b79aad428936c8544e19f60
pdr0 <yo@pedroselas.com>
2018-01-07 14:02:41 +0100

    init

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import Cell from './Cell';
+
+export function App() {
+    return(
+        <div class='app'>
+            hi
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Cell.js
@@ -0,0 +1,7 @@
+import { h } from 'preact';
+
+ export function Cell () {
+     console.log('Cell');
+ }
+
+ export default Cell;
